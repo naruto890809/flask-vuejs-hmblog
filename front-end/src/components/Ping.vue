6560381a165a40a0c5c4db911c3e6d50bde5b2b1
naruto890809 <705470427@qq.com>
2020-02-02 22:21:52 +0800

    axios

---
+++ front-end/src/components/Ping.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <button type="button" class="btn btn-primary">{{ msg }}</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Ping",
+  data: function() {
+    return {
+      msg: ""
+    };
+  },
+  methods: {
+    getMessage: function() {
+      const path = "http://127.0.0.1:5000/api/ping";
+      axios
+        .get(path)
+        .then(res => {
+          this.msg = res.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  },
+
+  created() {
+    this.getMessage();
+  }
+};
+</script>
+
+<style>
+</style>
