4324dd4eb6d26ba4de041ffced6f546476c25b1f
naruto890809 <705470427@qq.com>
2020-02-05 00:03:04 +0800

    3

--- back-end/hmblog.py
+++ back-end/hmblog.py
@@ -1,2 +1,8 @@
-from app import creat_app
+from app import creat_app,db
+from app.models import User
+
 app = creat_app()
+
+@app.shell_context_processor
+def make_shell_context():
+    return{'db':db,'User':User}
